9e3a66a2fa6731b46e5913f939e0666cfb6aa695
neven <nevendrean@yahoo.fr>
2020-12-19 00:56:57 +0100

    added fetch demo in route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
 
+/*************** VERSION NORMAL *********/
 const Nav = () => {
   function hand(e) {
     e.preventDefault();
@@ -38,7 +39,7 @@ const routes = [
   {
     path: "/b",
     // to protect the results
-    action: ({ mode }) => {
+    async action({ mode }) {
       if (mode === "A") {
         console.log("redirected to /A");
         return { redirect: "/" };
@@ -60,7 +61,75 @@ const routes = [
   },
 ];
 
-// change the value of mode: if "A", B -> A, else B->B
+/****** VERSION NEXT PB REDIRECT *********/
+const NavNext = ({ children }) => {
+  function hand(e) {
+    e.preventDefault();
+    history.push({ pathname: e.target.pathname });
+  }
+  return (
+    <div>
+      <a href="/" onClick={hand}>
+        A
+      </a>{" "}
+      |{" "}
+      <a href="/b" onClick={hand}>
+        B
+      </a>
+      <hr />
+      {children}
+    </div>
+  );
+};
+
+const routerNext = new UniversalRouter([
+  {
+    path: "",
+    async action({ next }) {
+      const component = await next(true);
+      return component && <NavNext>{component}</NavNext>;
+    },
+    children: [
+      {
+        path: "/",
+        async action() {
+          return (
+            <>
+              <br />
+              <p>A</p>
+            </>
+          );
+        },
+      },
+      {
+        path: "/b",
+        async action({ mode }) {
+          if (mode === "A") {
+            return { redirect: "" }; // <- no
+            /* but repeating the code works of course
+            return (
+              <>
+                <br />
+                <p> A</p>
+              </>
+            );
+            */
+          }
+          return (
+            <>
+              <br />
+              <p> B </p>
+            </>
+          );
+        },
+      },
+    ],
+  },
+]);
+
+/*************/
+
+// change the value of mode: protected if "A"
 const router = new UniversalRouter(routes, { context: { mode: "B" } });
 
 const root = document.getElementById("root");
@@ -68,6 +137,7 @@ const root = document.getElementById("root");
 async function renderRoute(location) {
   try {
     const page = await router.resolve({ pathname: location.pathname });
+    //                  ^^   change to routerNext to see pb
     if (page.redirect) {
       return history.push({ pathname: page.redirect });
     }
